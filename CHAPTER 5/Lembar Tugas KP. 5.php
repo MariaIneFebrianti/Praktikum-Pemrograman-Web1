2626580390b698918aae7c9ab78116ff083c516c
MariaIneFebrianti <132965857+MariaIneFebrianti@users.noreply.github.com>
2023-05-10 12:44:22 +0700

    Perubahan

---
+++ CHAPTER 5/Lembar Tugas KP. 5.php
@@ -0,0 +1,16 @@
+<?php
+    $nama = "Maria Ine Febarianti";
+    $lahir = "Cilacap, 26 April 1991";
+    $alamat = "Jl. Kalimantan, Cilacap";
+    $agama = "Islam";
+    $pekerjaan = "Mahasiswa";
+    $status = "Perjaka";
+
+    echo "<h3> BIODATA SAYA ADALAH SEBAGAI BERIKUT </h3>";
+    echo "Nama Lengkap          : " . $nama . "<br>";
+    echo "Tempat/Tanggal Lahir  : " . $lahir . "<br>";
+    echo "Alamat                : " . $alamat . "<br>";
+    echo "Agama                 : " . $agama . "<br>";
+    echo "Pekerjaan             : " . $pekerjaan . "<br>";
+    echo "Status                : " . $status . "<br>";
+?>
